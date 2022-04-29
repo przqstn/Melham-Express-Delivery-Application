0b1fa59eb5445cb4986e72673c81ec3820a65543
Kurt <Kvremolacio20@gmail.com>
2022-04-29 13:34:07 +0800

    MCC Delivery App Updated/Commit to Git

---
+++ app/src/main/java/com/example/mcc_deliveryapp/SignUp/SignUpDashboard.java
@@ -0,0 +1,16 @@
+package com.example.mcc_deliveryapp.SignUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mcc_deliveryapp.R;
+
+public class SignUpDashboard extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sign_up_dashboard);
+	}
+}
