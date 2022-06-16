b7da7f3d54228f4fc729e4b7c1b9e20e1c7596aa
LivingOrganism20 <Kvremolacio20@gmail.com>
2022-06-16 14:55:09 +0800

    add font style - alegraya and avenir add colors in xml splash screen
    05/05/2022
    tab layout user sign up and sign in done
    
    June 16, 2022
    Create login rider form done

---
+++ app/src/main/java/com/example/mcc_deliveryapp/Rider/riderLogin.java
@@ -0,0 +1,16 @@
+package com.example.mcc_deliveryapp.Rider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mcc_deliveryapp.R;
+
+public class riderLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rider_login);
+    }
+}
