75962f4c9af42709be6d59245dacd0e5cfe98531
Adrian <rdrgzadr@gmail.com>
2022-08-05 17:05:45 +0800

    removed unnecessary files

--- app/src/main/java/com/example/mcc_deliveryapp/Rider/rider_dashboard.java
+++ app/src/main/java/com/example/mcc_deliveryapp/Rider/rider_dashboard.java
@@ -31,24 +31,24 @@ public class rider_dashboard extends AppCompatActivity {
 		setContentView(R.layout.activity_rider_dashboard);
 
 
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		bottomNavigationView = findViewById(R.id.bottomNav_rider);
-		getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,courierHomeFragment).commit();
+		getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, courierHomeFragment).commit();
 		bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
 			@Override
-			public boolean onNavigationItemSelected( MenuItem item) {
-				switch (item.getItemId()){
+			public boolean onNavigationItemSelected(MenuItem item) {
+				switch (item.getItemId()) {
 					case R.id.iconhome:
-						getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,courierHomeFragment).commit();
+						getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, courierHomeFragment).commit();
 						return true;
 					case R.id.iconpickup:
-						getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,pickupFragment).commit();
+						getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, pickupFragment).commit();
 						return true;
 					case R.id.iconrecord:
-						getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,record_fragment).commit();
+						getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, record_fragment).commit();
 						return true;
 					case R.id.iconprofile:
-						getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,profile_fragment).commit();
+						getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, profile_fragment).commit();
 						return true;
 				}
 
@@ -56,57 +56,5 @@ public class rider_dashboard extends AppCompatActivity {
 			}
 		});
 
-//
-//		ImageButton button = (ImageButton) findViewById(R.id.btn_pickup);
-//		button.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				openActivity2();
-//			}
-//		});
-
-//		firebaseDatabase = FirebaseDatabase.getInstance();
-//
-//		// below line is used to get
-//		// reference for our database.
-//		databaseReference = firebaseDatabase.getReference("user parcel infos");
-//
-//		getdata();
 	}
-	public void openActivity2(){
-		Intent intent = new Intent(this,rider_dashboard1.class);
-		startActivity(intent);
-	}
-
-	// Hide both the navigation bar and the status bar.
-// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-// a general rule, you should design your app to hide the status bar whenever you
-// hide the navigation bar.
-
-//	public void getdata(){
-//		databaseReference.addValueEventListener(new ValueEventListener() {
-//			@Override
-//			public void onDataChange(@NonNull DataSnapshot snapshot) {
-//				// this method is call to get the realtime
-//				// updates in the data.
-//				// this method is called when the data is
-//				// changed in our Firebase console.
-//				// below line is for getting the data from
-//				// snapshot of our database.
-//				String value = snapshot.getValue(String.class);
-//
-//				// after getting the value we are setting
-//				// our value to our text view in below line.
-//				.setText(value);
-//			}
-//
-//			@Override
-//			public void onCancelled(@NonNull DatabaseError error) {
-//				// calling on cancelled method when we receive
-//				// any error or we are not able to get the data.
-//				Toast.makeText(rider_dashboard.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
-//			}
-//		});
-//	}
-//	}
 }
