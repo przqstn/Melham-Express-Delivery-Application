b01b8354cbab17df37fd2b376daaf11f457873b8
hmdmrndng-cvsu <hamodi.marandang@cvsu.edu.ph>
2022-08-09 09:41:08 +0800

    Added <activity/> in AndroidManifest.xml Added user_editprofile_fragment.java file Added btn_editProfile function in user_profile_fragment.java

---
+++ app/src/main/java/com/example/mcc_deliveryapp/User/user_editprofile_fragment.java
@@ -0,0 +1,17 @@
+package com.example.mcc_deliveryapp.User;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.mcc_deliveryapp.R;
+
+public class user_editprofile_fragment extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_user_editprofile_fragment);
+
+
+    }
+}
+
+
