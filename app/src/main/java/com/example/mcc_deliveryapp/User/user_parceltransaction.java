4084e415e89b37309221f9adac3beb1c4cd640c9
Jiiroo19 <jhonheron.carsocho@gmail.com>
2022-08-10 11:15:54 +0800

    Revised code to get the location od rider and user. Changed required permission of the application (removed the background location request) Added check of permission on take_order.java

--- app/src/main/java/com/example/mcc_deliveryapp/User/user_parceltransaction.java
+++ app/src/main/java/com/example/mcc_deliveryapp/User/user_parceltransaction.java
@@ -432,46 +432,9 @@ public class user_parceltransaction extends FragmentActivity implements Location
 			mMap.moveCamera(CameraUpdateFactory
 					.newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
 			mMap.getUiSettings().setMyLocationButtonEnabled(true);
-//			requestPermission();
-//			createLocationRequest();
-//			LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
-//			SettingsClient client = LocationServices.getSettingsClient(this);
-//			Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
-//			task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
-//				@Override
-//				public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-//					requestLoc();
-//				}
-//			});
-//
-//			task.addOnFailureListener(this, new OnFailureListener() {
-//				private static final int REQUEST_CHECK_SETTINGS = 0x1;
-//
-//				@Override
-//				public void onFailure(@NonNull Exception e) {
-//					if (e instanceof ResolvableApiException) {
-//						// Location settings are not satisfied, but this can be fixed
-//						// by showing the user a dialog.
-//						try {
-//							// Show the dialog by calling startResolutionForResult(),
-//							// and check the result in onActivityResult().
-//							ResolvableApiException resolvable = (ResolvableApiException) e;
-//							resolvable.startResolutionForResult(user_parceltransaction.this, REQUEST_CHECK_SETTINGS);
-//						} catch (IntentSender.SendIntentException sendEx) {
-//							// Ignore the error.
-//						}
-//					}
-//				}
-//			});
 		}
 	}
-//
-//	protected void createLocationRequest() {
-//		LocationRequest locationRequest = LocationRequest.create();
-//		locationRequest.setInterval(10000);
-//		locationRequest.setFastestInterval(5000);
-//		locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-//	}
+
 
 	@Override
 	protected void onPause() {
