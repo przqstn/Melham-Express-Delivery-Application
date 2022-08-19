76ea1e8410a18df12f2be600361c1c99e1b0cbb8
_jonsonkurt <jonsonkurt@gmail.com>
2022-08-19 11:57:28 +0800

    Added track order function before user sign-in

--- app/src/main/java/com/example/mcc_deliveryapp/MainActivity2.java
+++ app/src/main/java/com/example/mcc_deliveryapp/MainActivity2.java
@@ -1,27 +1,44 @@
 package com.example.mcc_deliveryapp;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mcc_deliveryapp.Rider.riderLogin;
 import com.example.mcc_deliveryapp.User.MainUserActivity;
+import com.example.mcc_deliveryapp.User.user_checkrate;
+import com.example.mcc_deliveryapp.User.user_ongoing_order_details2;
+import com.example.mcc_deliveryapp.User.user_order_summary;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 public class MainActivity2 extends AppCompatActivity {
-	Button btnriderform,btnuserform;
+	Button btnriderform,btnuserform,btntrack;
 	ProgressDialog progressDialog;
 	Timer timer;
+	EditText orderID;
+	String userDefaultNumber, ridernum;
 	private long pressedTime;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +47,7 @@ public class MainActivity2 extends AppCompatActivity {
 
 		btnriderform = findViewById(R.id.btn_riderform);
 		btnuserform = findViewById(R.id.btn_userform);
+		btntrack = findViewById(R.id.btn_trackorder);
 
 		// This callback will only be called when MyFragment is at least Started.
 		OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
@@ -80,6 +98,69 @@ public class MainActivity2 extends AppCompatActivity {
 						progressDialog.dismiss();
 					}
 				},5000);
+			}
+		});
+
+		btntrack.setOnClickListener(new View.OnClickListener() {
+			@SuppressLint("SetTextI18n")
+			@Override
+			public void onClick(View view) {
+				final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+						MainActivity2.this,R.style.BottomSheetDialogTheme
+				);
+				View bottomSheetView = LayoutInflater.from(getApplicationContext())
+						.inflate(
+								R.layout.trackorderbutton_dialog,
+								findViewById(R.id.trackorder_button)
+						);
+
+				orderID = bottomSheetView.findViewById(R.id.OrderID);
+
+
+				final FirebaseDatabase database = FirebaseDatabase.getInstance();
+				final DatabaseReference dr = database.getReference().child("userparcel");
+				Query query = dr.orderByChild("OrderID").equalTo(orderID.getText().toString());
+				System.out.println(orderID);
+
+				query.addChildEventListener(
+						new ChildEventListener() {
+							@Override
+							public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+								userDefaultNumber = dataSnapshot.child("defaultUserNum").getValue(String.class);
+								ridernum = dataSnapshot.child("ridernum").getValue(String.class);
+							}
+
+							@Override
+							public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+							}
+
+							@Override
+							public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+							}
+
+							@Override
+							public void onCancelled(@NonNull DatabaseError error) {
+
+							}
+
+							@Override
+							public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+							}
+						});
+
+				bottomSheetView.findViewById(R.id.btnConfirm).setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						Intent intent = new Intent(MainActivity2.this, user_ongoing_order_details2.class);
+						intent.putExtra("phonenum", userDefaultNumber);
+						intent.putExtra("orderID", orderID.getText().toString());
+						intent.putExtra("ridernum", ridernum);
+						startActivity(intent);
+					}
+				});
+				bottomSheetDialog.setContentView(bottomSheetView);
+				bottomSheetDialog.show();
 			}
 		});
 	}
