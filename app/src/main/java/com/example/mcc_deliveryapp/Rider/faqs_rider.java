f61ec8c6f69baed8855700414454d5839c47562d
Arjhann Estandante <estandante.arjhann@gmail.com>
2022-09-20 15:54:20 +0800

    - Created java and xml file for frequently asked questions for rider. - Comment out error code in Java File.

--- app/src/main/java/com/example/mcc_deliveryapp/Rider/faqs_rider.java
+++ app/src/main/java/com/example/mcc_deliveryapp/Rider/faqs_rider.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class rider_faqs extends AppCompatActivity {
+public class faqs_rider extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       /** setContentView(R.layout.activity_rider_faqs); **/
+        /** setContentView(R.layout.activity_faqs_rider); **/
     }
 }
